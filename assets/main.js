a16e1f2624f241d3f832df9feca40c61fd3487e2
John Brock <johnlrbrock@gmail.com>
2017-07-31 21:09:52 -0400

    added tests for valid game  grid

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,52 @@
 // Creates width X height grids filled with ' '
 var grid = function(width, height) {
-  // TODO: change this so all errors are at top and use guard statements
-  if (Number.isInteger(width) && Number.isInteger(height)) {
-    if (width < 1) {
-      console.log("Can't create grid with width less than 1");
-      width = 1;
-    }
-    if (height < 1) {
-      console.log("Can't create grid with height less than 1")
-      height = 1;
-    }
-
-    // create empty grid and row
-    let grid = [];
-    let row = [];
-    // populate row with spaces
-    for (let i = 0; i < width; i++ ) { 
-      row.push(' ');
-    };
-    // populate grid with rows
-    for (let i = 0; i < height; i++) {
-      grid.push(row);
-    };
-    return grid;
-  } else {
+  // validate inputs
+  if (!Number.isInteger(width) || !Number.isInteger(height)) {
     console.log("Grid's arguments weren't integers")
+    return false;
+  }
+  if (width < 1) {
+    console.log("Grid width to small. Set to 1.");
+    width = 1;
+  }
+  if (height < 1) {
+    console.log("Grid height to small. Set to 1.")
+    height = 1;
+  }
+
+  // create empty grid and row
+  let grid = [];
+  let row = [];
+  // populate row with spaces
+  for (let i = 0; i < width; i++ ) { 
+    row.push(' ');
+  };
+  // populate grid with rows
+  for (let i = 0; i < height; i++) {
+    grid.push(row);
+  };
+  return grid;
+};
+
+var validateGrid = function(grid, callerName) {
+  if (Array.isArray(grid) && grid.every(Array.isArray)) {
+    return true;
+  } else {
+    console.log(`invalid grid at ${callerName}`);
     return false;
   }
 };
 
 // set the content of the game area
 var setGameArea = function(grid) {
+  validateGrid(grid, 'setGameArea');
   $( '#game-area' ).html(parseGrid(grid));
   return true;
 };
 
 // accepts a two dimension grid of string and returns html
 var parseGrid = function(grid) {
+  validateGrid(grid, 'parseGrid');
   let html = '';
   for (let i = 0; i < grid.length; i++) {
     html = `${html} <div class="grid-row">`;
@@ -50,9 +60,7 @@ var parseGrid = function(grid) {
 
 // adds a grid of divs on the in the #game-area
 var render = function(grid) {
-  // is grid an array of array?
-  // TODO: abstract out grid test to function that accepts function name as argument
-  if (Array.isArray(grid) && grid.every(Array.isArray)) {
+  validateGrid(grid, 'render');
     if (setGameArea(grid)) {
       console.log('Game area rendered');
     }
