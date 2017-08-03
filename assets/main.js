61c6f7ddc6b6c2f8b7e44f19a8b2490cf501704a
John Brock <johnlrbrock@gmail.com>
2017-08-02 23:15:08 -0400

    linting

--- assets/main.js
+++ assets/main.js
@@ -4,55 +4,54 @@
 // TODO: Add date finished.
 
 // Creates width X height grids filled with ' '
-var grid = function(width, height) {
+const gridFactory = function (width, height) {
   // validate inputs
   if (!Number.isInteger(width) || !Number.isInteger(height)) {
-    console.log("Grid's arguments weren't integers")
+    console.log("Grid's arguments weren't integers");
     return false;
   }
   if (width < 1) {
-    console.log("Grid width to small. Set to 1.");
+    console.log('Grid width to small. Set to 1.');
     width = 1;
   }
   if (height < 1) {
-    console.log("Grid height to small. Set to 1.")
+    console.log('Grid height to small. Set to 1.');
     height = 1;
   }
 
   // create empty grid and row
-  let grid = [];
-  let row = [];
+  const grid = [];
+  const row = [];
   // populate row with spaces
-  for (let i = 0; i < width; i++ ) { 
+  for (let i = 0; i < width; i += 1) {
     row.push(' ');
-  };
+  }
   // populate grid with rows
-  for (let i = 0; i < height; i++) {
-    let rowCopy = [...row]
+  for (let i = 0; i < height; i += 1) {
+    const rowCopy = [...row];
     grid.push(rowCopy);
-  };
+  }
   return grid;
 };
 
-var validateGrid = function(grid, callerName) {
+const validateGrid = function (grid, callerName) {
   if (Array.isArray(grid) && grid.every(Array.isArray)) {
     return true;
-  } else {
-    console.log(`invalid grid at ${callerName}`);
-    return false;
   }
+  console.log(`invalid grid at ${callerName}`);
+  return false;
 };
 
 // accepts a two dimension grid of string and returns html
-var parseGrid = function(grid) {
-  if (!validateGrid(grid, 'parseGrid')){
+const parseGrid = function (grid) {
+  if (!validateGrid(grid, 'parseGrid')) {
     console.log("Can't parse grid.");
     return false;
   }
   let html = '';
-  for (let i = 0; i < grid.length; i++) {
+  for (let i = 0; i < grid.length; i += 1) {
     html = `${html} <div class="grid-row">`;
-    for (let j = 0; j < grid[i].length; j++) {
+    for (let j = 0; j < grid[i].length; j += 1) {
       html = `${html} <div class="grid-square">${grid[i][j]}</div>`;
     }
     html = `${html} </div>`;
@@ -60,67 +59,95 @@ var parseGrid = function(grid) {
   return html;
 };
 
+// add snake to grid
+const insertSnake = function (grid, snake) {
+  for (let i = 0; i < snake.length(); i += 1) {
+    const x = snake.coordinates[i][0] - 1;
+    const y = grid.length - snake.coordinates[i][1] - 1;
+    grid[y][x] = 'O';
+  }
+  return grid;
+};
+
+const insertFood = function (grid) {
+  return grid;
+};
+
 // set the content of the game area
-var setGameArea = function(grid) {
+const setGameArea = function (grid, snake) {
   if (!validateGrid(grid, 'setGameArea')) {
     console.log("Can't set game area");
     return false;
   }
-  $( '#game-area' ).html(parseGrid(grid));
+  grid = insertSnake(grid, snake);
+  grid = insertFood(grid);
+  $('#game-area').html(parseGrid(grid));
   return true;
 };
 
 // adds a grid of divs on the in the #game-area
-var render = function(grid) {
+const render = function (snake) {
+  const grid = gridFactory(40, 40);
   validateGrid(grid, 'render');
-  if (setGameArea(grid)) {
+  if (setGameArea(grid, snake)) {
     console.log('Game area rendered');
   }
 };
 
-var snakeFactory = function() {
-  console.log("We make snake.");
+const snakeFactory = function () {
+  console.log('We make snake.');
   return {
-    snakeCoordinates: [[20,20]],
     direction: 'r',
-    setDirection: function(dir) {
-      this.direction = dir;
+    coordinates: [[20, 20]],
+    length() {
+      return this.coordinates.length;
+    },
+    setDirection(dir) { this.direction = dir; },
+    moveRight() { this.coordinates[0][0] = this.coordinates[0][0] + 1; },
+    moveLeft()  { this.coordinates[0][0] = this.coordinates[0][0] - 1; },
+    moveUp()    { this.coordinates[0][1] = this.coordinates[0][1] - 1; },
+    moveDown()  { this.coordinates[0][1] = this.coordinates[0][1] - 1; },
+    move() {
+      switch(this.direction) {
+      case('l'):
+        this.moveLeft();
+        break;
+      case('r'):
+        this.moveRight();
+        break;
+      case('u'):
+        this.moveUp();
+        break;
+      case('d'):
+        this.moveDown();
+        break;
+      }
     },
   };
 };
 
-// TODO: remove
-// spawns a snake at (x,y)
-var spawnSnake = function(grid, x, y) {
-  if (!validateGrid(grid, 'parseGrid')){
-    console.log("Can't spawn snake.");
-    return false;
-  }
-  grid[grid.length - y - 1][x] = "O";
-  return grid;
-};
-
+// watches for arrow key presses
+// if there is one sets the snake's direction
 var watchForArrowKeys = function(snake) {
+  const left  = 37;
+  const up    = 38;
+  const right = 39;
+  const down  = 40;
     $( document ).keydown(function( event ) {
-    // left = 37
-    // up = 38
-    // right = 39
-    // down = 40
     switch (event.which) {
-    case 37:
+    case left:
       console.log('Player pressed left');
       snake.setDirection('l');
-      console.log(snake.direction)
       break;
-    case 38:
+    case up:
       console.log('Player pressed up');
       snake.setDirection('u');
       break;
-    case 39:
+    case right:
       console.log('Player pressed right');
       snake.setDirection('r');
       break;
-    case 40:
+    case down:
       console.log('Player pressed down');
       snake.setDirection('d');
       break;
@@ -128,11 +155,24 @@ var watchForArrowKeys = function(snake) {
   });
 };
 
+// cycles the game
+var turn = function(snake) {
+  let i = 0 
+  while (i < 10) {
+
+    setTimeout(function(){     
+      render(snake);
+      snake.move(); }
+      , 3000);
+    i++;
+  }
+};
+
 $( document ).ready(function() {
-  // instantiate grid
-  let gameGrid = grid(40,40);
-  render(gameGrid);
+  // make snake
   let snake = snakeFactory();
+  // control snake
   watchForArrowKeys(snake);
-  setTimeout(3000);
+  // let's wait a second
+  turn(snake);
 });
